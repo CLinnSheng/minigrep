c14e948d1ce9e30a3042cdae949e4dce37011cb6
linnsheng <linnsheng2002@gmail.com>
2026-08-01 22:52:20 +0800

    added new unit test to handle both case sensitive and insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,50 @@ use std::{error::Error, fs};
 
 pub fn run(target: String, file_path: String) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(file_path)?;
+
+    for line in search(&target, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
-fn search<'a>(target: &'a str, contents: &'a str) -> Vec<&'a str> {
-    unimplemented!()
+fn search<'a>(target: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut output = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(target) {
+            output.push(line);
+        }
+    }
+
+    output
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let target = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"], search(target, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let target = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive
+Pick three.
+Trust Me.";
+
+        assert_eq!(vec!["Rust", "Trust Me."], search(target, contents));
+    }
 }
